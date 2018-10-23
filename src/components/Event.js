450fc8c9dd2b5c9b4435d4ca43e1832d11579afa
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-10-23 01:04:44 -0300

    Add Dialogo para Expandir Cartões, e Flat List (Ainda em implementação) para comentários

--- src/components/Event.js
+++ src/components/Event.js
@@ -22,7 +22,8 @@ export default class Event extends Component {
         date: 'de 22 a 24 de Outubro de 2018',
         image: true,
         imageurl: 'http://www.computacao.ufcg.edu.br/_/rsrc/1373053918398/departamento/informes/iisemanaacademicadacomputacaoufcg/Captura%20de%20Tela%202013-07-05%20%C3%A0s%2016.50.58.png',
-        coments: '12',
+        coments: [{ coment: "RoadTop", userid: "caiosbl", userphoto: 'https://trello-avatars.s3.amazonaws.com/8caa44c9195400719a12896ec4799f31/30.png' }, { coment: "Vamos", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }, { coment: "Partiu", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }]
+      
       },
 
       {
@@ -33,7 +34,7 @@ export default class Event extends Component {
         date: 'de 26 a 29 de Outubro de 2018',
         image: true,
         imageurl: 'http://paraibacvb.com.br/wp-content/uploads/2018/03/pythonnordeste2018destaque.png',
-        coments: '115',
+        coments: [{ coment: "RoadTop", userid: "caiosbl", userphoto: 'https://trello-avatars.s3.amazonaws.com/8caa44c9195400719a12896ec4799f31/30.png' }, { coment: "Vamos", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }, { coment: "Partiu", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }]
       },
       {
         key: '3',
@@ -43,7 +44,7 @@ export default class Event extends Component {
         date: 'de 05 a 10 de Novembro de 2018',
         image: true,
         imageurl: 'https://joaobidu.com.br/wp-content/uploads/2016/08/pano-preto-em-cima-vela-vermela-acesa-e-cartas-de-taro-espalhadas-550x367.jpg',
-        coments: '220',
+        coments: [{ coment: "RoadTop", userid: "caiosbl", userphoto: 'https://trello-avatars.s3.amazonaws.com/8caa44c9195400719a12896ec4799f31/30.png' }, { coment: "Vamos", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }, { coment: "Partiu", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }]
       },
 
       {
@@ -54,7 +55,7 @@ export default class Event extends Component {
         date: '30 de Outubro de 2018',
         image: true,
         imageurl: 'https://i.ytimg.com/vi/AKvzS0mMy-c/maxresdefault.jpg',
-        coments: '150',
+        coments: [{ coment: "RoadTop", userid: "caiosbl", userphoto: 'https://trello-avatars.s3.amazonaws.com/8caa44c9195400719a12896ec4799f31/30.png' }, { coment: "Vamos", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }, { coment: "Partiu", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }]
       },
       {
         key: '5',
@@ -64,7 +65,7 @@ export default class Event extends Component {
         date: '30 de Outubro de 2018',
         image: true,
         imageurl: 'https://t.tudocdn.net/319345',
-        coments: '320',
+        coments: [{ coment: "RoadTop", userid: "caiosbl", userphoto: 'https://trello-avatars.s3.amazonaws.com/8caa44c9195400719a12896ec4799f31/30.png' }, { coment: "Vamos", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }, { coment: "Partiu", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }]
       },
       {
         key: '6',
@@ -74,7 +75,7 @@ export default class Event extends Component {
         date: '45 de Outubro de 2018',
         image: false,
         text: 'O Roadsec é o maior evento hacker da América Latina, durante o evento são apresentadas diversas palestras e oficinas de hacking, segurança da informação, ti e tecnologia em geral, além é claro de reunir profissionais, empresas e pessoas com o mesmo interesse, ou seja, como o Roadsec acaba sendo uma ótima forma de conhecer pessoas com os mesmos interesses que você, ou quem sabe até mesmo conseguir uma vaga de emprego ou simplesmente um networking.',
-        coments: '320'
+        coments: [{ coment: "RoadTop", userid: "caiosbl", userphoto: 'https://trello-avatars.s3.amazonaws.com/8caa44c9195400719a12896ec4799f31/30.png' }, { coment: "Vamos", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }, { coment: "Partiu", userid: "matheusgr", userphoto: 'https://sites.google.com/site/matheusgr/_/rsrc/1454169440563/config/customLogo.gif?revision=5' }]
       }
     ]
 
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {
     Text,
+    FlatList,
     StyleSheet,
     Dimensions,
     TouchableOpacity,
@@ -8,6 +9,7 @@ import {
 
 import { Card, CardItem, Left, Right, Body, Thumbnail, Icon, Button, View } from 'native-base'
 import ProgressiveImage from '../components/ProgressiveImage';
+import DialogManager, { ScaleAnimation, DialogContent } from 'react-native-dialog-component';
 
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
@@ -28,6 +30,92 @@ export default class PostCard extends Component {
             </CardItem>
         );
     }
+
+    renderCard(widthCard) {
+        return (
+            <Card style={{ marginLeft: 0, flex: 0, width: widthCard }}>
+                <CardItem>
+                    <Left style={{ flex: 0.8 }}>
+                        <Thumbnail small source={{ uri: this.data.item.userphoto }} />
+                        <Body>
+                            <Text style={styles.titleText}>{this.data.item.title}</Text>
+                            <Text note style={styles.defaultText}>Local: {this.data.item.local}</Text>
+                            <Text note style={styles.defaultText}>Data: {this.data.item.date}</Text>
+                        </Body>
+                    </Left>
+                    <Right style={{ flex: 0.2 }}>
+                        <Icon type="MaterialIcons" name="report" button onPress={() => alert("Cliquei em denunciar")} />
+                    </Right>
+                </CardItem>
+
+                {this.data.item.image ? this.renderImage() : this.renderText()}
+
+                <CardItem>
+                    <Left>
+                        <Button transparent>
+                            <Icon name="chatbubbles" style={{ fontSize: 10, color: 'grey' }} />
+                            <Text note style={styles.iconText}> {this.data.item.coments.length} Comentários</Text>
+                        </Button>
+                    </Left>
+                    <Right>
+                        <Icon type="MaterialCommunityIcons" name="delete" button onPress={() => alert("Cliquei em delete")} />
+                    </Right>
+                </CardItem>
+            </Card>
+        );
+    }
+
+    renderComments(){
+        return(
+            <FlatList
+            data={this.data.item.coments}
+            keyExtractor={item => item.coment}
+            onEndReachedThreshold={1}
+            renderItem={({ item }) => {
+                return (
+                    <View style={styles.item}>
+
+                    <Card style={{ marginLeft: 0, flex: 0, width: DialogManager.viewportWidth }}>
+                    <CardItem>
+                        <Left style={{ flex: 0.8 }}>
+                            <Thumbnail small source={{ uri:item.userphoto }} />
+                            <Body>
+                            <Text style={styles.titleText}>{item.userid}</Text>
+                            <Text style={styles.text}>{item.coment}</Text>
+                            </Body>
+                        </Left>
+                        
+                    </CardItem>
+                    </Card>
+
+
+                    </View>
+                );
+            }}
+        />
+
+        );
+    }
+
+
+
+    renderDialog() {
+        DialogManager.show({
+            height: 500,
+            animationDuration: 200,
+            ScaleAnimation: new ScaleAnimation(),
+            children: (
+                <DialogContent>
+                    <View>
+                        {this.renderCard(DialogManager.viewportWidth)}
+                        {this.renderComments()}
+                    </View>
+                </DialogContent>
+            ),
+        }, () => {
+       
+        });
+    }
     renderText() {
         return (
             <CardItem bordered>
@@ -40,36 +128,8 @@ export default class PostCard extends Component {
 
     render() {
         return (
-            <TouchableOpacity activeOpacity={0.5} onPress={() => alert("Cliquei no card")}>
-                <Card style={{marginLeft: 0, flex: 0, width: viewportWidth}}>
-                    <CardItem>
-                        <Left style={{ flex: 0.8 }}>
-                            <Thumbnail small source={{ uri: this.data.item.userphoto }} />
-                            <Body>
-                                <Text style={styles.titleText}>{this.data.item.title}</Text>
-                                <Text note style={styles.defaultText}>Local: {this.data.item.local}</Text>
-                                <Text note style={styles.defaultText}>Data: {this.data.item.date}</Text>
-                            </Body>
-                        </Left>
-                        <Right style={{ flex: 0.2 }}>
-                            <Icon type="MaterialIcons" name="report" button onPress={() => alert("Cliquei em denunciar")} />
-                        </Right>
-                    </CardItem>
-
-                    {this.data.item.image ? this.renderImage() : this.renderText()}
-
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon name="chatbubbles" style={{ fontSize: 10, color: 'grey' }} />
-                                <Text note style={styles.iconText}> {this.data.item.coments} Comentários</Text>
-                            </Button>
-                        </Left>
-                        <Right>
-                            <Icon type="MaterialCommunityIcons" name="delete" button onPress={() => alert("Cliquei em delete")} />
-                        </Right>
-                    </CardItem>
-                </Card>
+            <TouchableOpacity activeOpacity={0.5} onPress={() => this.renderDialog()}>
+                {this.renderCard(viewportWidth)}
             </TouchableOpacity>
         );
     }
@@ -90,7 +150,7 @@ const styles = StyleSheet.create({
         fontFamily: 'ProductSans',
         textAlign: 'justify',
         margin: 5,
-        fontSize: 14    
+        fontSize: 14
     },
 
     iconText: {
