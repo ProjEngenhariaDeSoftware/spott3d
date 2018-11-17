d560de62dd90b5e4f035cd48a23c5496458c8652
bruno <bruno.meneses@ccc.ufcg.edu.br>
2018-11-17 15:30:58 -0300

    adicionado filtro para pesquisar usuarios ou posts

---
+++ src/components/AdmView.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+
+export default class AdmView extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Em construção.</Text>
+      </View>
+    );
+  }
+}
